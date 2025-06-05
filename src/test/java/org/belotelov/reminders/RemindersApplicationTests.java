93a544173d7c0314e668c82c4d0970c93edc9243
Максим Белотелов <maksim_belotelov@mail.ru>
2025-06-05 12:51:23 +0100

    init commit

---
+++ src/test/java/org/belotelov/reminders/RemindersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.belotelov.reminders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RemindersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
